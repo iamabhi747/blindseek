eb668edd8cd1f956bea53e1262348cb21350b412
Abhinandan Bhuse <i.am.abhi747@gmail.com>
2024-12-02 22:52:36 +0530

    added stuff in SessionControl react-icons setup

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 
-function Button({ children, onClick }) {
+function Button({ children, className, onClick }) {
   return (
-    <button onClick={onClick} className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-0 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
+    <button onClick={onClick} className={'text-white bg-blue-700 hover:bg-blue-800 focus:ring-0 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800' + className}>
         { children }
     </button>
   );
--- src/game/SessionControl.jsx
+++ src/game/SessionControl.jsx
@@ -1,3 +1,27 @@
+import { IoExit } from "react-icons/io5";
+import { FaCopy } from "react-icons/fa";
+
+const copyToClipboard = async (text) => {
+  try {
+    await navigator.clipboard.writeText(text);
+    return true;
+  } catch (err) {
+    const textArea = document.createElement('textarea');
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    try {
+      document.execCommand('copy');
+      textArea.remove();
+      return true;
+    } catch (err) {
+      textArea.remove();
+      return false;
+    }
+  }
+ };
+ 
+ export { copyToClipboard };
 
 function SessionControl({ sessionId }) {
   
@@ -6,8 +30,19 @@ function SessionControl({ sessionId }) {
     window.location.reload();
   }
 
+  function copySessionId() {
+    copyToClipboard(sessionId);
+  }
+
   return (
-    <div>
+    <div className="w-full h-full content-center p-2 flex flex-row gap-3" style={{color: "rgb(244, 244, 245)"}}>
+      <div className="w-full h-full content-center">{sessionId}</div>
+      <button className=" w-8 h-8 content-center" onClick={copySessionId}>
+        <FaCopy className="w-5 h-5" />
+      </button>
+      <button className=" w-8 h-8 content-center" onClick={leaveSession}>
+        <IoExit size={30} className="w-full h-full" />
+      </button>
     </div>
   )
 }
